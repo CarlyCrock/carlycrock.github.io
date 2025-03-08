64cd387b51efd7e63318dd665c3b7e3d5781eaae
CarlyCrock <carly.bingham@gmail.com>
2025-03-08 15:46:44 -0700

    commit changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const greeting = document.getElementById ("greeting");
 greeting.style.textAlign="center";
 greeting.style.marginTop="50px";
-const username = window.prompt ("what is your name?", "Jane");
-greeting.textContent = "Welcome to Messy Genes, " +username+ "!";
+
