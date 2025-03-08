facad2eb80c9612ae2560d218ae1210e6bdb6d71
Crock <carly.bingham@gmail.com>
2025-03-08 15:49:31 -0700

    Add files via upload
    commit about and contact changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const greeting = document.getElementById ("greeting");
 greeting.style.textAlign="center";
 greeting.style.marginTop="50px";
-
+const username = window.prompt ("what is your name?", "Jane");
+greeting.textContent = "Welcome to Messy Genes, " +username+ "!";
